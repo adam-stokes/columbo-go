5e0693d82252d8b51a834b153f00ed7f94ed2208
Adam Stokes <battlemidget@users.noreply.github.com>
2020-07-14 15:57:20 -0400

    add dest flag
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- cmd/columbo.go
+++ cmd/columbo.go
@@ -19,6 +19,12 @@ func main() {
 				Aliases: []string{"r"},
 				Usage:   "Load rules spec from `YAML` file",
 			},
+			&cli.StringFlag{
+				Name:    "outdir",
+				Aliases: []string{"o"},
+				Usage:   "Output `DIR` to store extracted files",
+			},
+
 		},
 		Action: func(c *cli.Context) error {
 			var rulesSpec rules.RulesSpec
@@ -28,16 +34,17 @@ func main() {
 				srcFile = c.Args().Get(0)
 			}
 
-			err := tarextract.Extract(srcFile, "/tmp/wtf")
+			outDir := c.String("outdir")
+			err := tarextract.Extract(srcFile, outDir)
 			if err != nil {
 				log.Fatal(err)
 			}
 			// glob extracted file looking for any other compressed files
 			// basically a max depth 1 search for now
-			matches, _ := filepath.Glob("/tmp/wtf/*tar*")
+			matches, _ := filepath.Glob(filepath.Join(outDir, "*tar*"))
 			for _, match := range matches {
 				log.Println("Found ", match)
-				err = tarextract.Extract(match, "/tmp/wtf")
+				err = tarextract.Extract(match, outDir)
 				if err != nil {
 					log.Println("Could not extract, ", err)
 					continue
